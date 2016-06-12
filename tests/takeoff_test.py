2c302ab18fed26e9a79f13b222d99916e4ec583e
Chris Woodfield <caw@twitter.com>
2016-06-12 16:50:37 -0500

    testing BGP neighbors

--- tests/takeoff_test.py
+++ tests/takeoff_test.py
@@ -8,14 +8,23 @@ class TakeOffTest(object):
     self.error = []
     self.debug = []
 
-  def _handle_success(self, output):
+  @classmethod
+  def factory(cls, connection_object, hostname, platform):
+    obj = cls(connection_object=connection_object,
+                hostname=hostname, platform=platform)
+    obj.output = []
+    obj.error = []
+    obj.debug = []
+    return obj
+
+  def _handle_success(self, output=[]):
     # Output can be single string or list of strings
     if isinstance(output, str):
       output = [output]
     else:
       assert isinstance(output, list)
 
-    self.output = output
+    self.output.extend(output)
     self.status = True
     return self.status
 
@@ -28,12 +37,12 @@ class TakeOffTest(object):
 
     # Error can be single string or list of strings
     if isinstance(error, str):
-      error = [error]
+      self.error = [error]
     else:
       assert isinstance(error, list)
 
-    self.output = output
-    self.error = error
+    self.output.extend(output)
+    self.error.extend(error)
     self.status = False
     return self.status
 
